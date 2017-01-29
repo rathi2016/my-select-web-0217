cac428167c2be16fdc38d7dd66261f3d59c9cde9
Rathi Nair-Das <rathind14@gmail.com>
2017-01-29 14:18:26 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  result = []
+    counter = 0
+
+while counter < array.size
+    current_element = array[counter]
+    result << current_element if yield(current_element)
+
+    counter += 1
+
+  end
+  result
 end
